620cd73375aedcc1beab954a573ca10195fef141
Józef Paszkiewicz <paszkiewicz.jozef@gmail.com>
2024-10-23 21:29:42 +0200

    Dzien 3.

--- StatsApp/StatsApp/Program.cs
+++ StatsApp/StatsApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var name = "Ewa";
+bool women = false;
+var age = 15;
+
+
+if (women && age < 30)
+{
+    Console.WriteLine("KOBIETA PONIŻEJ 30 LAT");
+}
+
+if(name=="Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+
+if (!women && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
