b19feeb40857d113c09c3ed8bc8b56af1a125152
Józef Paszkiewicz <paszkiewicz.jozef@gmail.com>
2024-10-25 14:30:23 +0200

    Zadanie dzień 5 - wersja 2.

--- StatsApp/StatsApp/Program.cs
+++ StatsApp/StatsApp/Program.cs
@@ -1,69 +1,61 @@
-﻿int number = 997004852;
+﻿int number = 19087668;
 string numberInString = number.ToString();
 char[] letters = numberInString.ToCharArray();
 
+int[] counter = new int[10];
 
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+foreach (int i in counter)
+{
+    counter[i] = 0;
+}
 
 foreach (char letter in letters)
 {
     if (letter == '0')
     {
-        counter0++;
+        counter[0]++;
     }
     else if (letter == '1')
     {
-        counter1++;
+        counter[1]++;
     }
     else if (letter == '2')
     {
-        counter2++;
+        counter[2]++;
     }
     else if (letter == '3')
     {
-        counter3++;
+        counter[3]++;
     }
     else if (letter == '4')
     {
-        counter4++;
+        counter[4]++;
     }
     else if (letter == '5')
     {
-        counter5++;
+        counter[5]++;
     }
     else if (letter == '6')
     {
-        counter6++;
+        counter[6]++;
     }
     else if (letter == '7')
     {
-        counter7++;
+        counter[7]++;
     }
     else if (letter == '8')
     {
-        counter8++;
+        counter[8]++;
     }
     else if (letter == '9')
     {
-        counter9++;
+        counter[9]++;
     }
 }
-Console.WriteLine("0->" + counter0);
-Console.WriteLine("1->" + counter1);
-Console.WriteLine("2->" + counter2);
-Console.WriteLine("3->" + counter3);
-Console.WriteLine("4->" + counter4);
-Console.WriteLine("5->" + counter5);
-Console.WriteLine("6->" + counter6);
-Console.WriteLine("7->" + counter7);
-Console.WriteLine("8->" + counter8);
-Console.WriteLine("9->" + counter9);
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i + "->" + counter[i]);
+}
+
+
