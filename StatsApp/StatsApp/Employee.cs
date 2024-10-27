ec4fbe4354468aeac9afea0110a8f0771802feb7
Józef Paszkiewicz <paszkiewicz.jozef@gmail.com>
2024-10-27 21:21:08 +0100

    Zadanie dzień 6.

---
+++ StatsApp/StatsApp/Employee.cs
@@ -0,0 +1,41 @@
+﻿
+namespace StatsApp
+{
+    class Employee
+    {
+
+        private List<int> score = new List<int>();
+
+        public static string Company = "HAP";
+        public Employee(string name, string lastname, int age)
+        {
+            this.Name = name;
+            this.LastName = lastname;
+            this.Age = age;
+
+        }
+
+        public string Name { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+        public void AddScore(int number)
+        {
+            this.score.Add(number);
+        }
+
+        public void theBestWorker()
+        {
+            
+        }
+
+    }
+}
--- StatsApp/StatsApp/Program.cs
+++ StatsApp/StatsApp/Program.cs
@@ -1,61 +1,63 @@
-﻿int number = 19087668;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToCharArray();
+﻿
+using StatsApp;
+using System.Diagnostics.Contracts;
 
-int[] counter = new int[10];
 
-foreach (int i in counter)
+Employee employee1 = new Employee("Józef", "Paszkiewicz", 34);
+Employee employee2 = new Employee("Adam", "Kowalski", 23);
+Employee employee3 = new Employee("Jan", "Nowak", 52);
+
+var rand = new Random();
+employee1.AddScore(rand.Next(1, 11));
+employee1.AddScore(3);
+employee1.AddScore(6);
+employee1.AddScore(2);
+employee1.AddScore(1);
+
+employee2.AddScore(rand.Next(1, 11));
+employee2.AddScore(3);
+employee2.AddScore(6);
+employee2.AddScore(2);
+employee2.AddScore(1);
+
+employee3.AddScore(rand.Next(1, 11));
+employee3.AddScore(3);
+employee3.AddScore(6);
+employee3.AddScore(2);
+employee3.AddScore(1);
+
+
+
+var resultEmployee1 = employee1.Result;
+var resultEmployee2 = employee2.Result;
+var resultEmployee3 = employee3.Result;
+Console.WriteLine(employee1.Name +" "+ employee1.LastName+": "+ resultEmployee1);
+Console.WriteLine(employee2.Name +" "+ employee2.LastName+": "+ resultEmployee2);
+Console.WriteLine(employee3.Name +" "+ employee3.LastName+": "+ resultEmployee3);
+
+Console.WriteLine(Employee.Company);
+//Console.WriteLine(employee1.score);
+
+
+
+List <Employee> employees = new List<Employee>()
 {
-    counter[i] = 0;
+    employee1, employee2, employee3
+};
+
+int maxResult = -1;
+Employee bestEmployee = null;
+
+foreach (var employee in employees)
+{
+    if (employee.Result > maxResult)
+    {
+        bestEmployee = employee;
+        maxResult = employee.Result;
+    }
 }
 
-foreach (char letter in letters)
-{
-    if (letter == '0')
-    {
-        counter[0]++;
-    }
-    else if (letter == '1')
-    {
-        counter[1]++;
-    }
-    else if (letter == '2')
-    {
-        counter[2]++;
-    }
-    else if (letter == '3')
-    {
-        counter[3]++;
-    }
-    else if (letter == '4')
-    {
-        counter[4]++;
-    }
-    else if (letter == '5')
-    {
-        counter[5]++;
-    }
-    else if (letter == '6')
-    {
-        counter[6]++;
-    }
-    else if (letter == '7')
-    {
-        counter[7]++;
-    }
-    else if (letter == '8')
-    {
-        counter[8]++;
-    }
-    else if (letter == '9')
-    {
-        counter[9]++;
-    }
-}
+Console.WriteLine("Najlepszy pracownik ma "+bestEmployee.Age + " lata i nazywa się "+ bestEmployee.Name +" "+bestEmployee.LastName);
 
-for (int i = 0; i < 10; i++)
-{
-    Console.WriteLine(i + "->" + counter[i]);
-}
 
 
